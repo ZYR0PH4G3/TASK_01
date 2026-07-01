42522bd6aa5fa172aa6ddd275a52a26c8941a709
ZYR0PH4G3 <shukla.d3stry0.arnav@gmail.com>
2026-07-02 00:14:54 +0530

    tried fixing the level 2 bug

--- Scenes/minigame_1.gd
+++ Scenes/minigame_1.gd
@@ -6,22 +6,36 @@ var garlic_collected = 0
 var timer_end = false 
 
 func _ready() -> void:
+	# Starts your 7-second visual countdown
 	await minigame_timer.Timer(7.0) 
 	timer_end = true
+	print("TIMER EXPIRED! Calculating final garlic score...")
 
 func _process(delta: float) -> void: 
-	if garlic_collected == 3: 
-		if Global.minigames_done > 3: 
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn") 
-		else:
-			get_tree().change_scene_to_file("res://scenes/level_scene.tscn") 
 	
-	# This shield stops the timer from stealing your life if you won!
-	if timer_end and garlic_collected < 3: 
-		Global.minigames_done -= 1 
-		Global.lives -= 1 
-		get_tree().change_scene_to_file("res://scenes/level_scene.tscn") 
+	# The game ONLY transitions when the 7-second timer runs completely out:
+	if timer_end: 
+		# WIN STATE: Player successfully got 3 or more garlic
+		if garlic_collected >= 3:
+			print("You won! Progressing to next stage...")
+			
+			# Tells the Global script that you beat a minigame!
+			Global.minigames_done += 1 
+			
+			# If they beat more than 3 minigames in total, go to the final win screen
+			if Global.minigames_done > 3: 
+				get_tree().change_scene_to_file("res://scenes/done_screen.tscn") 
+			else:
+				# Otherwise, go back to the intermission to load minigame 2!
+				get_tree().change_scene_to_file("res://scenes/level_scene.tscn") 
+		
+		# LOSE STATE: Player failed to get enough garlic
+		else:
+			print("You lost!")
+			Global.minigames_done -= 1 
+			Global.lives -= 1 
+			get_tree().change_scene_to_file("res://scenes/level_scene.tscn") 
 
 func garlic_collect() -> void: 
 	garlic_collected = garlic_collected + 1
-	print("Garlic collected! Total: ", garlic_collected)
+	print("Garlic collected! Current Total: ", garlic_collected)
--- minigame_2.gd
+++ minigame_2.gd
@@ -6,18 +6,29 @@ var buttons_pressed := 0
 var timer_end = false
 
 func _ready() -> void:
+	# Starts your 7-second visual countdown
 	await themed_timer.Timer(7.0)
 	timer_end = true 
 
 func _process(delta: float) -> void:
-	if buttons_pressed == 4:
-		if Global.minigames_done > 3:
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
-		else:
-			get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
 	
-	# This shield stops the timer from stealing your life if you won!
-	if timer_end and buttons_pressed < 4:
-		Global.lives -= 1
-		Global.minigames_done -= 1
-		get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+	# The game ONLY transitions when the 7-second timer runs completely out:
+	if timer_end:
+		# WIN STATE: Player successfully pressed all 4 buttons
+		if buttons_pressed == 4:
+			print("You won the button game!")
+			
+			# Tells the Global script that you beat a minigame!
+			Global.minigames_done += 1
+			
+			if Global.minigames_done > 3:
+				get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
+			else:
+				get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
+		
+		# LOSE STATE: Player failed to press all 4 buttons in time
+		else:
+			print("You lost the button game!")
+			Global.lives -= 1
+			Global.minigames_done -= 1
+			get_tree().change_scene_to_file("res://scenes/level_scene.tscn")
